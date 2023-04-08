582b68fdfbcb96a186172a884f1d0574fda21ae3
AnzorTori <eziotheking@gmail.com>
2023-04-08 20:24:29 +0300

    bluetooth screen
    1) Created a Bluetooth like screen which scans the area for other devices
    2) Added filter option by mac address

--- app/src/main/java/com/example/mapapp/ui/bluetoothscreen/BluetoothFragment.kt
+++ app/src/main/java/com/example/mapapp/ui/bluetoothscreen/BluetoothFragment.kt
@@ -1,17 +1,67 @@
 package com.example.mapapp.ui.bluetoothscreen
 
+import android.bluetooth.BluetoothManager
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.mapapp.R
+import androidx.core.content.ContextCompat.getSystemService
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.mapapp.databinding.FragmentBluetoothBinding
+import com.example.mapapp.ui.bluetoothscreen.blescanner.adapter.BleDeviceAdapter
+import com.example.mapapp.ui.bluetoothscreen.blescanner.model.BleDevice
+import com.example.mapapp.ui.bluetoothscreen.blescanner.model.BleScanCallback
 
 class BluetoothFragment : Fragment() {
 
+
+    private lateinit var binding: FragmentBluetoothBinding
+    private lateinit var btManager: BluetoothManager
+    private lateinit var bleScanManager: BleScanManager
+    private lateinit var foundDevices: MutableList<BleDevice>
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_bluetooth, container, false)
+    ): View {
+        binding = FragmentBluetoothBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupView()
+    }
+
+    private fun setupView() {
+
+        foundDevices = BleDevice.createBleDevicesList(binding.editTextFilter.text.toString())
+        val adapter = BleDeviceAdapter(foundDevices)
+        binding.rvFoundDevices.adapter = adapter
+        binding.rvFoundDevices.layoutManager = LinearLayoutManager(context)
+        getSystemService(requireContext(), BluetoothManager::class.java)?.let { manager ->
+            btManager = manager
+        }
+        bleScanManager = BleScanManager(btManager, 5000, scanCallback = BleScanCallback({
+            val name = it?.device?.address
+            if (name.isNullOrBlank()) return@BleScanCallback
+
+            val device = BleDevice(name)
+            if (!foundDevices.contains(device)) {
+                foundDevices.add(device)
+                adapter.notifyItemInserted(foundDevices.size - 1)
+            }
+        }))
+
+        bleScanManager.beforeScanActions.add { binding.btnStartScan.isEnabled = false }
+        bleScanManager.beforeScanActions.add {
+            foundDevices.clear()
+            adapter.notifyDataSetChanged()
+        }
+        bleScanManager.afterScanActions.add { binding.btnStartScan.isEnabled = true }
+
+        binding.btnStartScan.setOnClickListener {
+            bleScanManager.scanBleDevices()
+        }
     }
 }
